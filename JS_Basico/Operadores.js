7bc97b4d6102e39a13d4fecc9c229cb391fdaa57
Deiwin-Ignacio-Monsalve-Altamar <1651@holbertonschool.com>
2020-06-15 01:40:13 +0000

    update operadores

---
+++ JS_Basico/Operadores.js
@@ -0,0 +1,51 @@
+/*-ARITMETICOS / BINARIOS: +(suma), -(resta), * (multiplicación), / (división)
+Binarios porque es entre dos valores
+
+    -
+    UNITY: !(negación)
+Un solo operador con un solo valor
+    -
+    ASIGNACION: = (igual)
+
+-
+COMPARACIÓN == (comparar) NOTA: puede comparar entre diferentes tipos, como numeros y strings(el valor perse) ===
+    (comparar) Compara que los tipos y los valores sean iguales <
+    (menor que) >
+    (mayor que) <=
+    (mejor o igual) >=
+    (mayor o igual) -
+    LOGICOS && (and) ||
+    (or)
+
+++Incrementar en 1 un valor
+    += 2 Incrementar un valor en 2*/
+
+//Operadores de operacion o operadores binarios
+3 + 2
+50 - 10
+10 * 20
+20 / 2 "Ramon " + "Ruiz"
+
+//Operadores unitarios
+!false
+
+//Operadores de asignacion
+var a = 1;
+
+//Operadores de comparacion
+3 == "3" //true
+3 === "3" //false
+5 < 3
+5 > 3
+5 <= 6
+5 >= 6
+
+//Operadores Logicos
+a && b
+a || b
+
+//Operadores de incremento
+var edad = 40
+edad++
+edad += 2 ``
+`
