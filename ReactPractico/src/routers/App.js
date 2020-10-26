59d96110428b35d77544b58ff1837aa9121211e2
Deiwin Monsalve <deiwin1987@gmail.com>
2020-10-26 08:40:45 -0500

    cagreagndo nuevas funcionalidades a APP

--- ReactPractico/src/routers/App.js
+++ ReactPractico/src/routers/App.js
@@ -6,18 +6,17 @@ import Login from '../containers/Login';
 import NotFound from '../containers/NotFound';
 import Register from '../containers/Registrer';
 import Layout from '../componets/Layout'; 
+import Player from '../containers/Player';
 
 const App = () => (
     <BrowserRouter>
-        <Layout>
             <Switch>
                 <Route exact path='/' component={Home} />
                 <Route exact path='/login' component={Login} />
                 <Route exact path='/register' component={Register} />
+                <Route exact path='/player/:id' component={Player} />
                 <Route component={NotFound} />
             </Switch>
-
-        </Layout>
     </BrowserRouter>
 );
 
