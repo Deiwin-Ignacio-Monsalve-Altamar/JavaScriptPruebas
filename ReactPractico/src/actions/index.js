d1ba60ddfb1a5d6258462b6f7c553173c0efad73
Deiwin Monsalve <deiwin1987@gmail.com>
2020-10-26 08:43:20 -0500

    cagregando actions para utilizar redux

---
+++ ReactPractico/src/actions/index.js
@@ -0,0 +1,34 @@
+export const setFavorite = payload =>({ // Retornamos y exportamos la función un objeto
+	type: 'SET_FAVORITE', // Este es el nombre/descripción que le vamos a dar a nuestro action
+	payload // El payload se le podría llamar como la acción de la UI
+})
+
+export const deleteFavorite = payload => ({
+	type: 'DELETE_FAVORITE',
+	payload
+})
+
+export const loginRequest = payload =>({
+	type: 'LOGIN_REQUEST',
+	payload,
+});
+
+export const lagoutRequest = payload =>({
+	type: 'LOGOUT_REQUEST',
+	payload,
+});
+
+export const registerRequest = payload =>({
+	type: 'REGISTER_REQUEST',
+	payload
+});
+
+export const getVideoSource = payload =>({
+	type: 'GET_VIDEO_SOURCE',
+	payload,
+});
+
+export const searchVideo = (payload) => ({
+	type: 'SEARCH_VIDEO',
+	payload,
+  });
